9a7717073b74cf87cf78be5dc4ffc9139562901c
Matthew Russell <matthewjohnrussell@gmail.com>
2018-05-09 14:57:54 +0100

    minimal drivers for param estimation testing

---
+++ drivers/minimal_1.cpp
@@ -0,0 +1,181 @@
+#include <advection_diffusion_reaction_problem.h>
+#include <config_file.h>
+
+#include <fstream>
+
+using namespace mjrfd;
+
+struct ChemokinesParams
+{
+    double pe_u;
+};
+
+enum Variable
+{
+    c_u = 0,
+};
+
+class ChemokinesProblem1D : public AdvectionDiffusionReactionProblem
+{
+public:
+    ChemokinesProblem1D(const unsigned n_node, const double dt) :
+        AdvectionDiffusionReactionProblem(1, n_node, dt)
+    {
+        enable_bc(Boundary::Left,  { c_u });
+        enable_bc(Boundary::Right, { c_u });
+
+        set_variable_names({ "c_u" });
+
+        Max_residual = 1.0e-14;
+    }
+
+    ~ChemokinesProblem1D()
+    {
+    }
+
+    void set_initial_conditions()
+    {
+        // Set everything to zero
+        clear_solution();
+
+        // Set c_u to 1.0 at the left-hand boundary
+        u(0, c_u, 0) = 1.0;
+    }
+
+    ChemokinesParams p;
+
+private:
+    const std::unordered_map<int, double>& stencil_1_helper(unsigned i) const
+    {
+        if(i == 0)
+            return stencil::forward_1::weights;
+        else if(i == n_node_-1)
+            return stencil::backward_1::weights;
+        else
+            return stencil::central_1::weights;
+    }
+
+    void get_bc(Boundary b,
+                std::vector<double> &a1,
+                std::vector<double> &a2,
+                std::vector<double> &a3) const override
+    {
+        if(b == Boundary::Left)
+        {
+            a1[c_u] = 1.0; a2[c_u] = 0.0; a3[c_u] = 1.0;
+        }
+        if(b == Boundary::Right)
+        {
+            a1[c_u] = 1.0; a2[c_u] = 0.0; a3[c_u] = 0.0;
+        }
+    }
+
+    void get_d(const unsigned t,
+               const unsigned i,
+               std::vector<double> &d) const override
+    {
+        d[c_u]   = 1.0;
+    }
+
+    void get_v(const unsigned i,
+               const unsigned var,
+               double &v) const override
+    {
+        v = p.pe_u;
+    }
+
+    void get_dv_du(const unsigned i,
+                   const unsigned var,
+                   const unsigned i2,
+                   const unsigned var2,
+                   double &dv_du) const override
+    {
+        dv_du = 0.0;
+    }
+
+    void get_r(const std::vector<double> &u,
+               std::vector<double> &r) const override
+    {
+        r[c_u] = 0.0;
+    }
+
+    void get_dr_du(const std::vector<double> &u,
+                   std::vector<std::vector<double>> &dr_du) const override
+    {
+        dr_du[c_u][c_u] = 0.0;
+    }
+};
+
+int main(int argc, char **argv)
+{
+    if(argc != 5 && argc != 6)
+    {
+        std::cerr << "Usage: " << argv[0]
+                  << " config_file n_node dt t_max [ output_interval ]\n";
+        std::exit(1);
+    }
+
+    const unsigned n_node = std::atoi(argv[2]);
+    const double dt = std::atof(argv[3]);
+    const double t_max = std::atof(argv[4]);
+
+    unsigned output_interval = 1;
+
+    if(argc == 6)
+    {
+        output_interval = std::atoi(argv[5]);
+    }
+
+    ChemokinesProblem1D problem(n_node, dt);
+
+    std::ifstream config_file(argv[1]);
+    ConfigFile cf(config_file);
+    cf.print_all();
+
+    problem.p.pe_u = cf.get<double>("pe_u");
+
+    problem.enable_terse_logging();
+
+    char filename[200];
+    std::ofstream outfile;
+
+    // perform a steady solve and output it
+    problem.steady_solve();
+    std::sprintf(filename, "output_steady.csv");
+    outfile.open(filename);
+    problem.output(outfile);
+    outfile.close();
+
+    // set initial conditions
+    problem.set_initial_conditions();
+
+    // output initial conditions
+    std::sprintf(filename, "output_%05i.csv", 0);
+    outfile.open(filename);
+    problem.output(outfile);
+    outfile.close();
+
+    unsigned i = 1;
+
+    // timestepping loop
+    while(problem.time() <= t_max)
+    {
+        // solve for current timestep
+        problem.unsteady_solve();
+
+        if(i % output_interval == 0)
+        {
+            // output current solution
+            //std::cout << "Outputting solution at time = " << problem.time() << '\n';
+            std::cout << ";\tOutputting";
+            std::sprintf(filename, "output_%05i.csv", i/output_interval);
+            outfile.open(filename);
+            problem.output(outfile);
+            outfile.close();
+        }
+
+        ++i;
+    }
+
+    std::cout << "\n\nReached t > t_max (" << t_max << ") after performing " << i-1 << " timesteps\n";
+}
