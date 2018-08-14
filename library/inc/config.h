96ebcd2e29881b20ac520ec3c0cef96a55bd28f4
Matthew Russell <matthewjohnrussell@gmail.com>
2018-08-14 12:45:57 +0100

    Tidy up Config class and add comments
    Should be no change to usage

--- library/inc/config.h
+++ library/inc/config.h
@@ -11,40 +11,37 @@
 
 namespace mjrfd
 {
-    // Simple configuration file loader and parser. Currently supports
-    // ini-style key-value pairs, ignoring whitespace within each line, e.g.:
-    // a=2.4
-    // b = 4.134
+    /// Simple command-line arguments and configuration file loader and parser.
+    /// Items are added to a map in the order they are found. This allows to
+    /// override the values in the config file with the cmdline, say.
+    /// Currently supports long-style cmdline args, e.g. "--key value", and
+    /// ini-style key-value pairs, ignoring whitespace within each line, e.g.:
+    /// a=2.4
+    /// b = 4.134
     class Config
     {
     public:
-        Config()
-        {
-        }
+        /// Default constructor
+        Config() = default;
 
-        // Constructor to immediately parse the command line arguments
+        /// Constructor to immediately parse the command line arguments
         Config(int argc, char **argv)
         {
             parse_command_line(argc, argv);
         }
 
-        // Constructor to immediately parse an istream of the config file
+        /// Constructor to immediately parse an istream of the config file
         Config(std::istream &is)
         {
             parse_config_file(is);
         }
 
-        // Delete default copy constructor and assignment operator to prevent copying
+        /// Delete default copy constructor and assignment operator to prevent copying
         Config(const Config &) = delete;
         Config& operator=(const Config&) = delete;
 
-        // Empty destructor
-        ~Config()
-        {
-        }
-
-        // Get the value associated with key from the parameters, interpreted
-        // as templated type T. Must be specialised for each required type.
+        /// Get the value associated with key from the parameters, interpreted
+        /// as templated type T. Must be specialised for each required type.
         template<class T>
         const T get(const std::string &key)
         {
@@ -55,8 +52,8 @@ namespace mjrfd
             return get_impl<T>(key);
         }
 
-        // Read the config file (or any istream) and parse its contents,
-        // storing the params in a map
+        /// Read the config file (or any istream) and parse its contents,
+        /// adding the params to the map
         void parse_config_file(std::istream &is)
         {
             // storage for a line of the config file
@@ -93,6 +90,12 @@ namespace mjrfd
             }
         }
 
+        /// Read and parse the command-line arguments adding the params to the
+        /// map. Currently limited to simple "--key value" pairs of long-style
+        /// options which always have values. Ignores items in argv until it
+        /// finds an argument starting with "--" (allows drivers to have custom
+        /// positional arguments without much hassle here), and then all
+        /// arguments are assumed to be of the above format
         void parse_command_line(int argc, char **argv)
         {
             // Find the index of the first command line argument that starts with "--"
@@ -136,7 +139,7 @@ namespace mjrfd
             }
         }
 
-        // Print all key-value pairs to stream out
+        /// Print all key-value pairs to stream out
         void print_all(std::ostream &out = std::cout) const
         {
             out << "Recognised parameters:\n";
@@ -160,10 +163,10 @@ namespace mjrfd
         }
 
     private:
-        // Map parameter names to values
+        /// Map parameter names to values
         std::unordered_map<std::string, std::string> params_;
 
-        // Helper function for removing all whitespace characters from a string
+        /// Helper function for removing all whitespace characters from a string
         static void remove_whitespace(std::string &s)
         {
             s.erase(std::remove_if(s.begin(),
@@ -172,13 +175,13 @@ namespace mjrfd
                     s.end());
         }
 
-        // Function to perform actual conversion to templated type - must be
-        // specialised for each type required
+        /// Function to perform actual conversion to templated type - must be
+        /// specialised for each type required
         template<class T>
         const T get_impl(const std::string &key);
     };
 
-    // Specialise get_impl for double
+    /// Specialise get_impl for double
     template<>
     const double Config::get_impl(const std::string &key)
     {
@@ -186,7 +189,7 @@ namespace mjrfd
         return std::atof(params_[key].c_str());
     }
 
-    // Specialise get_impl for bool
+    /// Specialise get_impl for bool
     template<>
     const bool Config::get_impl(const std::string &key)
     {
@@ -194,7 +197,7 @@ namespace mjrfd
         return static_cast<bool>(std::atoi(params_[key].c_str()));
     }
 
-    // Specialise get_impl for string
+    /// Specialise get_impl for string
     template<>
     const std::string Config::get_impl(const std::string &key)
     {
