802a437414fa1aa215f16a4c0659a52fa06f0c84
Matthew Russell <matthewjohnrussell@gmail.com>
2018-08-28 21:53:42 +0100

    put test inside mjrfd namespace

--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -6,70 +6,29 @@
 #include <sstream>
 #include <string>
 
-TEST_CASE( "Configuration file and command line arguments", "[config]" )
+namespace mjrfd
 {
-    mjrfd::Config config;
 
-    SECTION( "Configuration from file" )
+    TEST_CASE( "Configuration file and command line arguments", "[config]" )
     {
-        std::ifstream file("../../tests/test_config.ini");
-        config.parse_config_file(file);
+        mjrfd::Config config;
 
-        REQUIRE( config.get<double>("a") == 1.3 );
-        REQUIRE( config.get<bool>("b") == false );
-        REQUIRE( config.get<bool>("c") == true );
-        REQUIRE( config.get<std::string>("d") == "AString" );
-
-        SECTION( "Override configuration file with command line" )
+        SECTION( "Configuration from file" )
         {
-            int argc = 3;
-            const char * const argv[] = { "test_config", "--a", "4.6" };
-
-            config.parse_command_line(argc, const_cast<char **>(argv));
-
-            REQUIRE( config.get<double>("a") == 4.6 );
-            REQUIRE( config.get<bool>("b") == false );
-            REQUIRE( config.get<bool>("c") == true );
-            REQUIRE( config.get<std::string>("d") == "AString" );
-        }
-    }
-
-    SECTION( "Configuration from stringstream" )
-    {
-        std::stringstream config_ss;
-        config_ss << "a = 1.3\n"
-                  << "b = 0\n"
-                  << "c = 1\n"
-                  << "d = AString";
-
-        config.parse_config_file(config_ss);
-
-        REQUIRE( config.get<double>("a") == 1.3 );
-        REQUIRE( config.get<bool>("b") == false );
-        REQUIRE( config.get<bool>("c") == true );
-        REQUIRE( config.get<std::string>("d") == "AString" );
-    }
-
-    SECTION( "Configuration from command line arguments" )
-    {
-        SECTION( "Only \"--key value\" arguments" )
-        {
-            int argc = 9;
-            const char * const argv[] = { "test_config", "--a", "1.3", "--b", "0", "--c", "1", "--d", "AString" };
-
-            config.parse_command_line(argc, const_cast<char **>(argv));
+            std::ifstream file("../../tests/test_config.ini");
+            config.parse_config_file(file);
 
             REQUIRE( config.get<double>("a") == 1.3 );
             REQUIRE( config.get<bool>("b") == false );
             REQUIRE( config.get<bool>("c") == true );
             REQUIRE( config.get<std::string>("d") == "AString" );
 
-            SECTION( "Override command line with configuration file" )
+            SECTION( "Override configuration file with command line" )
             {
-                std::stringstream config_ss;
-                config_ss << "a = 4.6";
+                int argc = 3;
+                const char * const argv[] = { "test_config", "--a", "4.6" };
 
-                config.parse_config_file(config_ss);
+                config.parse_command_line(argc, const_cast<char **>(argv));
 
                 REQUIRE( config.get<double>("a") == 4.6 );
                 REQUIRE( config.get<bool>("b") == false );
@@ -78,17 +37,62 @@ TEST_CASE( "Configuration file and command line arguments", "[config]" )
             }
         }
 
-        SECTION( "Positional then \"--key value\" arguments" )
+        SECTION( "Configuration from stringstream" )
         {
-            int argc = 12;
-            const char * const argv[] = { "test_config", "should", "be", "ignored", "--a", "1.3", "--b", "0", "--c", "1", "--d", "AString" };
+            std::stringstream config_ss;
+            config_ss << "a = 1.3\n"
+                      << "b = 0\n"
+                      << "c = 1\n"
+                      << "d = AString";
 
-            config.parse_command_line(argc, const_cast<char **>(argv));
+            config.parse_config_file(config_ss);
 
             REQUIRE( config.get<double>("a") == 1.3 );
             REQUIRE( config.get<bool>("b") == false );
             REQUIRE( config.get<bool>("c") == true );
             REQUIRE( config.get<std::string>("d") == "AString" );
+        }
+
+        SECTION( "Configuration from command line arguments" )
+        {
+            SECTION( "Only \"--key value\" arguments" )
+            {
+                int argc = 9;
+                const char * const argv[] = { "test_config", "--a", "1.3", "--b", "0", "--c", "1", "--d", "AString" };
+
+                config.parse_command_line(argc, const_cast<char **>(argv));
+
+                REQUIRE( config.get<double>("a") == 1.3 );
+                REQUIRE( config.get<bool>("b") == false );
+                REQUIRE( config.get<bool>("c") == true );
+                REQUIRE( config.get<std::string>("d") == "AString" );
+
+                SECTION( "Override command line with configuration file" )
+                {
+                    std::stringstream config_ss;
+                    config_ss << "a = 4.6";
+
+                    config.parse_config_file(config_ss);
+
+                    REQUIRE( config.get<double>("a") == 4.6 );
+                    REQUIRE( config.get<bool>("b") == false );
+                    REQUIRE( config.get<bool>("c") == true );
+                    REQUIRE( config.get<std::string>("d") == "AString" );
+                }
+            }
+
+            SECTION( "Positional then \"--key value\" arguments" )
+            {
+                int argc = 12;
+                const char * const argv[] = { "test_config", "should", "be", "ignored", "--a", "1.3", "--b", "0", "--c", "1", "--d", "AString" };
+
+                config.parse_command_line(argc, const_cast<char **>(argv));
+
+                REQUIRE( config.get<double>("a") == 1.3 );
+                REQUIRE( config.get<bool>("b") == false );
+                REQUIRE( config.get<bool>("c") == true );
+                REQUIRE( config.get<std::string>("d") == "AString" );
+            }
         }
     }
 }
