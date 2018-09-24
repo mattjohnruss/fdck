9b8b58651bb39ae9cc333e1e2ed5c3145983112c
Matthew Russell <matthewjohnrussell@gmail.com>
2018-09-24 09:06:24 +0100

    fix old adv_diff_1D driver

--- drivers/adv_diff_1D.cpp
+++ drivers/adv_diff_1D.cpp
@@ -5,6 +5,11 @@
 
 using namespace mjrfd;
 
+enum Variable
+{
+    c = 0,
+};
+
 class DiffusionProblem1D : public Problem
 {
     typedef Eigen::Triplet<double> T;
@@ -13,42 +18,46 @@ public:
     // treat all nodes as dofs in this example
     // we could eliminate the first and last nodes since they are Dirichlet
     // BCs, but this will require a sophisticated dof numbering scheme etc
-    DiffusionProblem1D(const unsigned n_node, const double dt) :
-        Problem(n_node, dt),
+    DiffusionProblem1D(const unsigned n_node) :
+        Problem(1, n_node),
         n_node_(n_node),
         dx_(1.0/(n_node-1))
     {
         std::cout << "n_node = " << n_node_ << '\n';
         std::cout << "dx = " << dx_ << '\n';
-        std::cout << "dt = " << dt_ << '\n';
 
         // set zero initial conditions
         for(unsigned i = 0; i < n_node_; ++i)
         {
-            u(i) = 0.0;
+            u(c,i) = 0.0;
         }
+
+        Max_residual = 1e-14;
     }
 
     ~DiffusionProblem1D()
     {
     }
 
-    void output(std::ostream &out) const
+    void output(std::ostream &out) const override
     {
+        out << "t x c\n";
         for(unsigned i = 0; i < n_node_; ++i)
         {
-            out << static_cast<double>(i)/static_cast<double>(n_node_-1) << ' '
-                << u(i) << '\n';
+            out << time() << ' '
+                << static_cast<double>(i)/static_cast<double>(n_node_-1) << ' '
+                << u(c,i) << '\n';
         }
     }
 
-    void output_exact(std::ostream &out) const
+    void output_exact(std::ostream &out) const override
     {
+        out << "t x c\n";
         for(unsigned i = 0; i < n_node_; ++i)
         {
             double x = static_cast<double>(i)/static_cast<double>(n_node_-1);
 
-            out << x << " " << 1.0 - x << '\n';
+            out << time() << " " << x << " " << 1.0 - x << '\n';
         }
     }
 
@@ -56,35 +65,35 @@ private:
     unsigned n_node_;
     double dx_;
 
-    void calculate_residual()
+    void calculate_residual() override
     {
         // set all entries to zero
         // not neccessary here since all entries are set explicitly
         //residual_.setZero();
 
-        residual_(0) = u(0)*dx_*dx_;
+        residual_(0) = u(c,0)*dx_*dx_;
 
         for(unsigned i = 1; i < n_node_-1; ++i)
         {
             // time derivative
-            residual_(i) = -(u(i) - u(1,i))*dx_*dx_/dt_;
+            residual_(i) = -(u(c,i) - u(1,c,i))*dx_*dx_/dt_;
 
             // diffusion (u_ and u_old_ because of Crank-Nicolson)
-            residual_(i) += 0.5*(u(i-1) - 2*u(i) + u(i+1));
-            residual_(i) += 0.5*(u(1,i-1) - 2*u(1,i) + u(1,i+1));
+            residual_(i) += 0.5*(u(c,i-1) - 2*u(c,i) + u(c,i+1));
+            residual_(i) += 0.5*(u(1,c,i-1) - 2*u(1,c,i) + u(1,c,i+1));
 
             // advection (u_ and u_old_ because of Crank-Nicolson)
-            residual_(i) -= 0.5*(6.0*(-0.5*u(i-1) + 0.5*u(i+1)))*dx_;
-            residual_(i) -= 0.5*(6.0*(-0.5*u(1,i-1) + 0.5*u(1,i+1)))*dx_;
+            residual_(i) -= 0.5*(6.0*(-0.5*u(c,i-1) + 0.5*u(c,i+1)))*dx_;
+            residual_(i) -= 0.5*(6.0*(-0.5*u(1,c,i-1) + 0.5*u(1,c,i+1)))*dx_;
 
             // forcing
-            residual_(i) += time_*8.0*dx_*dx_;
+            residual_(i) += 8.0*dx_*dx_;
         }
 
-        residual_(n_node_-1) = u(n_node_-1)*dx_*dx_;
+        residual_(n_node_-1) = u(c,n_node_-1)*dx_*dx_;
     }
 
-    void calculate_jacobian()
+    void calculate_jacobian() override
     {
         // set all entries to zero
         //jacobian_.setZero();
@@ -113,14 +122,13 @@ int main()
     const unsigned n_node = 1001;
     const double dt = 0.001;
 
-    DiffusionProblem1D problem(n_node, dt);
-    DiffusionProblem1D::Max_residual = 1e-14;
+    DiffusionProblem1D problem(n_node);
 
     char filename[200];
     std::ofstream outfile;
 
     // output initial conditions
-    std::sprintf(filename, "output_%i.dat", 0);
+    std::sprintf(filename, "output_%05i.csv", 0);
     outfile.open(filename);
     problem.output(outfile);
     outfile.close();
@@ -130,12 +138,12 @@ int main()
     while(problem.time() < 1.0)
     {
         // solve for current timestep
-        problem.unsteady_solve();
+        problem.unsteady_solve(dt);
 
         if(i % 10 == 0)
         {
             // output current solution
-            std::sprintf(filename, "output_%i.dat", i/10);
+            std::sprintf(filename, "output_%05i.csv", i/10);
             outfile.open(filename);
             problem.output(outfile);
             outfile.close();
