1e839820ec2dd0bc5d5f6218c3c522d48fdc4b8c
Matthew Russell <matthewjohnrussell@gmail.com>
2019-02-02 15:16:20 +0000

    add template args to mesh lerp; move to header

--- library/inc/utilities.h
+++ library/inc/utilities.h
@@ -55,10 +55,68 @@ namespace mjrfd
 
         double lerp(double s, double v0, double v1);
 
-        void lerp_mesh(const Eigen::Ref<Eigen::VectorXd> &x,
-                       const Eigen::Ref<Eigen::VectorXd> &v,
-                       const Eigen::Ref<Eigen::VectorXd> &x_interp,
-                       Eigen::Ref<Eigen::VectorXd> v_interp);
+        /// Resample data v defined at points x at the new set of points
+        /// x_interp, using linear interpolation. Templated to avoid
+        /// difficulties with binding Ref<VectorXd>& to "col"s from a
+        /// Map<...> that can be either row- or column-major. We only need to
+        /// call e.g. x(i), v(i) etc. in the function body.
+        template<class V1, class V2, class V3>
+        void lerp_mesh(const V1 &x,
+                       const V2 &v,
+                       const V3 &x_interp,
+                       Eigen::Ref<Eigen::VectorXd> v_interp)
+        {
+            // get the number of points in the original grid
+            const unsigned n = x.size();
+
+            // check it matches the number of data points
+            assert(n == v.size());
+
+            // get the number of points in the new grid
+            const unsigned n_interp = x_interp.size();
+
+            // check it matches the number of data points
+            assert(n_interp == v_interp.size());
+
+            // loop over the points in x_interp
+            for(unsigned i = 0; i < n_interp; ++i)
+            {
+                // Binary search:
+                // Start the search at the (approx) middle of the original grid.
+                // There are n-1 cells, so the index of the last cell is n-2
+                unsigned cell = 0;
+
+                {
+                    unsigned l_cell = 0;
+                    unsigned r_cell = n - 2;
+
+                    // we assume this can't fail
+                    while(l_cell <= r_cell)
+                    {
+                        cell = (l_cell + r_cell)/2;
+
+                        if(x_interp(i) > x(cell+1))
+                        {
+                            l_cell = cell + 1;
+                        }
+                        else if(x_interp(i) < x(cell))
+                        {
+                            r_cell = cell - 1;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                // After the loop, cell is the index of the interval that
+                // contains x_interp(i)
+                // Now do the actual lerp:
+                const double s = (x_interp(i) - x(cell))/(x(cell+1) - x(cell));
+                v_interp(i) = lerp(s, v(cell), v(cell+1));
+            }
+        }
 
         std::tuple<std::vector<double>, unsigned, unsigned>
             read_csv_to_flat_vector(std::istream &is, char delimiter = ' ');
